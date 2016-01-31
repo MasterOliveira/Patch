6ecc40c85c6dcaec32f31ea5493ac74f8e65dd8c
MasterOliveira <cassiofoliveira@gmail.com>
2016-01-31 18:11:03 -0300

    Created a new route to cliente/js in the server. Refactored 'HelloApp' to a separate file. New Feature: User can add and remove targets from the 'targetList'.

--- client/js/controllers/hello-controller.js
+++ client/js/controllers/hello-controller.js
@@ -1,5 +1,17 @@
-var app = angular.module("hello", []);
 
-app.controller("helloController", function($scope) {
+app.controller('helloController', ['$scope','$resource', 
+function($scope, $reource) {
 	$scope.helloTarget = "world";
-})
+	$scope.targets = []
+
+	$scope.addTarget = function() {
+		$scope.targets.push({ name: $scope.targetName});
+		$scope.targetName = "";
+	}
+
+	$scope.removeTarget = function(targetIndex) {
+		$scope.targets.splice(targetIndex,1);
+	}
+
+}]);
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use('/controllers', express.static(__dirname + '/client/js/controllers'));
+app.use('/js', express.static(__dirname + '/client/js'));
 
 app.get('/', function(req,res) { 
 	res.sendFile(__dirname + '/client/views/index.html');
