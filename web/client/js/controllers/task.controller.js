c863946fea6064adeab609518f2c7c0d6f733d16
MasterOliveira <cassiofoliveira@gmail.com>
2016-04-18 02:12:36 -0300

    renamed patch-web to web, and added the first mocha chai test

---
+++ web/client/js/controllers/task.controller.js
@@ -0,0 +1,58 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('efficientApp')
+		.controller('todoController', TodoController);
+
+	TodoController.$inject = ['taskService'];
+
+	function TodoController(taskService) {
+		var vm = this;
+
+		vm.globalTarget = "World";
+		vm.tasks = [];
+
+		vm.initialize = initialize
+		vm.removeTask = removeTask
+		vm.addTask = addTask
+
+		initialize()
+		
+		function initialize() {
+			listTasks()
+			console.debug('TodoController was Activated!')
+		}
+
+		function listTasks() {
+			taskService.listTasks()
+				.then(function(data) {
+					vm.tasks = data;
+					console.debug(data)
+				});
+		};
+
+		function addTask() {
+			var task = {};
+			task.name = vm.taskName;
+			
+			taskService.addTask(task)
+				.then(function(data) {
+					vm.tasks.push(data);
+					console.debug(data)
+				});
+				
+		};
+
+		function removeTask(taskIndex) {
+			var taskId = vm.tasks[taskIndex]._id
+			taskService.removeTask(taskId)
+				.then(function(data) {
+					vm.tasks.splice(taskIndex,1);
+					console.debug(data)
+				});
+		};
+
+	}
+})();
+
---
+++ web/test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('First test', function () {
+	it('should pass if the test config is working', function () {
+		expect(true).to.be.true;
+	})
+})
