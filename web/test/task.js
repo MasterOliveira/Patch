17c144dad0bb7fc4ab8f8cb51fd280ef8b144ccc
MasterOliveira <cassiofoliveira@gmail.com>
2016-04-24 21:28:04 -0300

    Attempting to make Karma work.Added Karma and a couple of plugins.

--- web/test/task.js
+++ web/test/task.js
@@ -1,11 +1,11 @@
-var expect 				= require('chai').expect
-	, mongoose			= require('mongoose')
-	, mockgoose			= require('mockgoose')
-	, httpMock			= require('node-mocks-http')
-	, taskController 	= require('./server/controllers/task.controller');
+// var expect 				= require('chai').expect
+// 	, mongoose			= require('mongoose')
+// 	, mockgoose			= require('mockgoose')
+// 	, httpMock			= require('node-mocks-http')
+// 	, taskController 	= require('./server/controllers/task.controller');
 
-describe('Task', function () {
-	it('should store input in mongo', function () {
-		expect(true).to.be.true;
-	})
-})
+// describe('Task', function () {
+// 	it('should store input in mongo', function () {
+// 		expect(true).to.be.true;
+// 	})
+// })
--- web/test/test.js
+++ web/test/test.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect;
 
-describe('Infrastruture Test', function () {
+describe('Basic Config', function () {
+	beforeEach(module('efficientApp'));
+
 	it('Should pass if the default config is working.', function () {
 		expect(true).to.be.true;
 	})
